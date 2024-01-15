89792cc39ca96dfb2d9abe3514c2f7609fa56b5f
tegmen88 <cite7173@gmail.com>
2024-01-15 14:59:55 +0100

    search

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 let movieList = document.getElementById("movieList")
 let movieInfo = document.getElementById("movieInfo")
 
-
-
-console.log ("hejsan");
 fetch("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=88d6f906b386ac47c004701d8f545df8")
 .then(res => res.json())
 .then(data=> {
-printMovieList(data)
+// printMovieList(data)
 })
 
 function printMovieList (movies) {
@@ -41,9 +38,28 @@ function printMovieInfo(movie){
     movieText.innerText = movie.overview;
  
     let movieImg =document.createElement("img")
- movieImg.style.width ="500px"
+    movieImg.style.width ="500px"
     movieImg.src ="https:/image.tmdb.org/t/p/original/" + movie.poster_path;
  
     movieDiv.append(movieHeadline, movieText, movieImg)
     movieInfo.appendChild(movieDiv)
 }
+
+// Search
+function searchMovie(search){
+    let movieSearch = document.getElementById("searchMovie").value;
+
+    movieList.innerHTML="";
+
+    fetch('https://api.themoviedb.org/3/search/movie?query=' + movieSearch + '&api_key=88d6f906b386ac47c004701d8f545df8')
+      .then(response => response.json())
+      .then(data=> {
+        printMovieList(data)
+    })
+
+    console.log(movieSearch);
+}
+
+
+
+
